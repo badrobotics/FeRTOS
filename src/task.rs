e4daceb0395b5dfd9923cd9e89306911fc529f94
Bijan Tabatabai <bijan311@yahoo.com>
2019-03-27 13:07:55 -0500

    Made an enum for the sp element of the Task struct
    This is to hopefully reduce the number of casts that
    had to be used before.
    
    Signed-off-by: Bijan Tabatabai <bijan311@yahoo.com>

--- src/task.rs
+++ src/task.rs
@@ -4,20 +4,27 @@ use rust_tm4c123::interrupt;
 
 #[repr(C)]
 #[derive(Copy, Clone)]
+union StackPtr {
+    reference: &'static u32,
+    ptr: *const u32,
+    num: u32
+}
+
+#[repr(C)]
+#[derive(Copy, Clone)]
 struct Task {
     //Stack pointer
-    sp: *const u32,
+    sp: StackPtr,
     //Entry point into task
     ep: fn()
 }
 
 const MAX_TASKS: usize = 10;
 const INIT_XPSR: u32 = 0x01000000;
-const IRQ_STACK_SIZE: u32 = 4 * 16;
 const IDLE_STACK_SIZE: usize = 64;
 
 static mut IDLE_STACK: [u32; IDLE_STACK_SIZE] = [0; IDLE_STACK_SIZE];
-static mut TASKS: [Task; MAX_TASKS] = [Task { sp: &0, ep: idle }; MAX_TASKS];
+static mut TASKS: [Task; MAX_TASKS] = [Task { sp: StackPtr {num: 0}, ep: idle }; MAX_TASKS];
 static mut NUM_TASKS: usize = 0;
 static mut NEXT_TASK: usize = 0;
 static mut CUR_TASK: usize = 7;
@@ -57,8 +64,8 @@ pub unsafe extern "C" fn sys_tick() {
 }
 
 //See section 2.5.7.1 and Figure 2-7 in the TM4C123 datasheet for more information
-unsafe fn set_initial_stack(stack_ptr: *const u32, entry_point: fn()) {
-    let mut cur_ptr = ((stack_ptr as u32) + IRQ_STACK_SIZE - 4) as *mut u32;
+unsafe fn set_initial_stack(stack_ptr: *const u32, entry_point: fn()) -> *const u32 {
+    let mut cur_ptr = ((stack_ptr as u32) - 4) as *mut u32;
 
     //Set the xPSR
     *cur_ptr = INIT_XPSR;
@@ -72,24 +79,29 @@ unsafe fn set_initial_stack(stack_ptr: *const u32, entry_point: fn()) {
     *cur_ptr = idle as u32;
     cur_ptr = (cur_ptr as u32 - 4) as *mut u32;
 
-    //Clear registers R12, R0-R3
+    //Clear all of the registers
     for _i in 0..13 {
         *cur_ptr = 0;
         cur_ptr = (cur_ptr as u32 - 4) as *mut u32;
     }
+
+    //In the for loop we subtracted 4 too many, so we have to add that back
+    (cur_ptr as u32 + 4) as *const u32
 }
 
 pub unsafe fn add_task(stack_ptr: &'static u32, stack_size: usize, entry_point: fn()) -> bool {
     if NUM_TASKS < MAX_TASKS {
         TASKS[NUM_TASKS] = Task {
-            //Arm uses a full descending stack so we have to start from the top
-            //and we also have to consider the data the IRQ handler expects to be
-            //at the top of the stack when we context switch.
-            sp: ((stack_ptr as *const u32 as u32) + (4 * (stack_size as u32)) - IRQ_STACK_SIZE) as *const u32,
+            sp: StackPtr {
+                    reference: stack_ptr
+                },
             ep: entry_point
         };
 
-        set_initial_stack(TASKS[NUM_TASKS].sp, entry_point);
+        //Arm uses a full descending stack so we have to start from the top
+        TASKS[NUM_TASKS].sp.num += 4 * (stack_size as u32);
+
+        TASKS[NUM_TASKS].sp.ptr = set_initial_stack(TASKS[NUM_TASKS].sp.ptr, entry_point);
 
         NUM_TASKS += 1;
 
