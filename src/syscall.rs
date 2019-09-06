5c06fed94fd042fb8102affd5708648caeb40777
Bijan Tabatabai <bijan311@yahoo.com>
2019-09-06 12:46:49 -0500

    Make sure the syscalls get linked
    Since they are not directly called by the rust code, they
    are in danger of being thrown away by the linker.
    
    Signed-off-by: Bijan Tabatabai <bijan311@yahoo.com>

--- src/syscall.rs
+++ src/syscall.rs
@@ -4,6 +4,10 @@ extern "C" {
     pub fn svc_handler();
 }
 
+//For the linker to link the syscalls, a function in this
+//file must be called from elsewhere...
+pub fn link_syscalls(){}
+
 #[no_mangle]
 extern "C" fn sys_exit() {
     unsafe { task::remove_task(); }
