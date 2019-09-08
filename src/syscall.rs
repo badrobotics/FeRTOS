1629b5f23719c16ae17e100bb947e4f47fd65923
Bijan Tabatabai <bijan311@yahoo.com>
2019-09-08 18:40:26 -0500

    Added ability for syscalls to return values
    This is done by overwritting the saved R0 register in the interrupt stack frame.
    Each syscall must return a value now to ensure that R0 does not contain data that
    would be overwritten.
    
    Signed-off-by: Bijan Tabatabai <bijan311@yahoo.com>

--- src/syscall.rs
+++ src/syscall.rs
@@ -1,4 +1,4 @@
-use super::task;
+use crate::task;
 
 extern "C" {
     pub fn svc_handler();
@@ -9,13 +9,17 @@ extern "C" {
 pub fn link_syscalls(){}
 
 #[no_mangle]
-extern "C" fn sys_exit() {
+extern "C" fn sys_exit() -> usize {
     unsafe { task::remove_task(); }
+
+    0
 }
 
 #[no_mangle]
-extern "C" fn sys_sleep(ms32: u32) {
+extern "C" fn sys_sleep(ms32: u32) -> usize {
     let ms: u64 = ms32 as u64;
     task::sleep(ms);
+
+    0
 }
 
