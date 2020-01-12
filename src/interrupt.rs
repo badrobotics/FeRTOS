5ab2e6ee6912328a2e3dd9235e7ba037c514dec3
dkitzman <dakota.kitzman@gmail.com>
2020-01-12 16:33:29 -0600

    cleaning interrupt registration up a bit

--- src/interrupt.rs
+++ src/interrupt.rs
@@ -1,7 +1,7 @@
-use core::ptr;
 use core::panic::PanicInfo;
+use core::ptr;
 
-pub fn int_register(int_num: u32, int_handler: unsafe extern "C" fn()) {
+pub fn int_register(irqn: i8, int_handler: unsafe extern "C" fn()) {
     //This is defined in the linker script
     extern "C" {
         static mut _svtable: u32;
@@ -9,23 +9,28 @@ pub fn int_register(int_num: u32, int_handler: unsafe extern "C" fn()) {
     }
 
     unsafe {
-        let ram_table_start = &_svtable as *const u32;
-        let ram_table_end = &_evtable as *const u32;
-        let num_interrupts = ((ram_table_end as u32) - (ram_table_start as u32)) / 4;
-        let int_pos = (ram_table_start as u32 + (4 * int_num)) as *mut u32;
+        // ARM exceptions are negative numbers. Add 16 to be zero indexed.
+        let int_num: usize = (irqn + 16) as usize;
+        let int_pos = ((&_svtable) as *const u32).add(int_num) as *mut u32;
+
+        //Collect pointer to SCB peripheral registers
         let regs = cortex_m::peripheral::SCB::ptr();
 
         //If the interrupt table has not been relocated to RAM, do so
-        if (*regs).vtor.read() != ram_table_start as u32 {
-            //I tried to use ptr::copy_nonoverlapping, but for some reason
-            //that didn't work...
-            for i in 0..num_interrupts {
-                let ram_pos = (ram_table_start as u32 + (4 * i as u32)) as *mut u32;
-                let flash_pos = ((*regs).vtor.read() + (4 * i as u32)) as *mut u32;
-                ptr::write(ram_pos, *flash_pos);
-            }
-            (*regs).vtor.write(ram_table_start as u32);
+        if (*regs).vtor.read() != _svtable as u32 {
+            // Number of interrupts in vector table
+            let count = (&_evtable as *const u32 as usize - &_svtable as *const u32 as usize) / 4;
+
+            // Copy vector table from flash to ram
+            let flash_ptr = (*regs).vtor.read() as *const u32;
+            let ram_ptr = (&mut _svtable) as *mut u32;
+            ptr::copy_nonoverlapping(flash_ptr, ram_ptr, count);
+
+            // point vector table to new locaiton
+            (*regs).vtor.write(ram_ptr as u32);
         }
+
+        // Register callback to irqn
         ptr::write(int_pos, int_handler as u32);
     }
 }
