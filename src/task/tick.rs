a918ef8254587c569ff56492149803e67de4418e
Bijan Tabatabai <bijan311@yahoo.com>
2020-01-22 20:07:59 -0600

    Made the task list into a linked list and made the TICKS var atomic
    Whoopsie, I made two features one commit...
    Also, it's not a problem, but now sometimes the parameter for the sleep
    task inexplicably gets set to like 3 billion, leaving the task blocked for
    much longer than expected. It's probs not a problem though.
    
    Signed-off-by: Bijan Tabatabai <bijan311@yahoo.com>

---
+++ src/task/tick.rs
@@ -0,0 +1,32 @@
+use core::sync::atomic::{AtomicU32, Ordering};
+
+pub struct TickCounter {
+    lsb: AtomicU32,
+    msb: AtomicU32,
+}
+
+impl TickCounter {
+    pub const fn new() -> Self{
+        TickCounter {
+            lsb: AtomicU32::new(0),
+            msb: AtomicU32::new(0),
+        }
+    }
+
+    pub fn inc(&self) {
+        let old_lsb = self.lsb.fetch_add(1, Ordering::SeqCst);
+
+        //If there was overflow, increment the lsb
+        if self.lsb.load(Ordering::SeqCst) < old_lsb {
+            let old_msb = self.msb.load(Ordering::SeqCst);
+            self.msb.compare_and_swap(old_msb, old_msb + 1, Ordering::SeqCst);
+        }
+    }
+
+    pub fn get(&self) -> u64 {
+        let lsb = self.lsb.load(Ordering::SeqCst);
+        let msb = self.msb.load(Ordering::SeqCst);
+
+        ((msb as u64) << 32) | (lsb as u64)
+    }
+}
